b718e0db112da614d0ce3921702c527f0118d451
sbx_user1051 <sbx_user1051@169.254.101.229>
2024-09-20 19:10:32 +0000

    Fri Sep 20 19:10:32 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,11 +4,18 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+	def calAprice(quantity):
+		res = 0
+		res += (quantity//5) * 200
+		res += ((quantity%5)//3) * 130
+		res += quantity %3 *50
+		return res
 	price_table = {
-		'A': lambda quantity: (quantity % 3) * 50 + (quantity // 3) * 130,
+		'A': calAprice,
 		'B': lambda quantity: (quantity % 2) * 30 + (quantity // 2) * 45,
 		'C': lambda quantity: quantity * 20,
-		'D': lambda quantity: quantity * 15
+		'D': lambda quantity: quantity * 15,
+		'E': lambda quantity: quantity * 40
 	}
 	if any([item not in price_table.keys() for item in skus]):
 		return -1
@@ -19,3 +26,4 @@ def checkout(skus):
 	for item, quantity in skus_cnt.items():
 		price += price_table[item](quantity)
 	return price
+
