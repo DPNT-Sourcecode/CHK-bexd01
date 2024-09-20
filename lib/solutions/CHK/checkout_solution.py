a6896fa5246c128aa03b7c714c248a50e8f979c1
sbx_user1051 <sbx_user1051@169.254.101.229>
2024-09-20 20:24:06 +0000

    Fri Sep 20 20:24:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,6 +27,22 @@ def checkout(skus):
 		'L': lambda quantity: calculate_price(quantity, [(1, 90)]),
 		'M': lambda quantity: calculate_price(quantity, [(1, 15)]),
 		'N': lambda quantity: calculate_price(quantity, [(1, 40)]),
+		'O': lambda quantity: calculate_price(quantity, [(1, 10)]),
+		'P': lambda quantity: calculate_price(quantity, [(5, 200),(1, 50)]),
+		'Q': lambda quantity: calculate_price(quantity, [(3, 80),(1, 30)]),
+		'R': lambda quantity: calculate_price(quantity, [(1, 50)]),
+	}
+	def cal_diff(sku1, sku2, sku1_bundle, skus_cnt):
+		price = 0
+		if skus_cnt.get(sku1, 0) >= sku1_bundle:
+			freeSKU2 = skus_cnt[sku1] // sku1_bundle
+			currSKU2 = skus_cnt.get(sku2, 0)
+			if currSKU2 > 0:
+				price -= price_table['B'](currSKU2)
+				price += price_table['B'](currSKU2 - freeSKU2)
+		return price
+	promotable = {
+		'E': lambda sku_q: cal_diff('E', 'B', 2, sku_q)
 	}
 	if any([item not in price_table.keys() for item in skus]):
 		return -1
@@ -37,16 +53,17 @@ def checkout(skus):
 	
 	for item, quantity in skus_cnt.items():
 		price += price_table[item](quantity)
-	
-	# Todo make it unifrom
-	if skus_cnt.get('E', 0) >= 2:
-		freeB = skus_cnt['E'] // 2
-		currB = skus_cnt.get('B', 0)
-		if currB > 0:
-			price -= price_table['B'](currB)
-			price += price_table['B'](currB - freeB)
+		if item in promotable:
+			price += promotable[item](skus_cnt)
+
 	if skus_cnt.get('F', 0) > 2:
 		currF = skus_cnt['F']
 		price -= price_table['F']((currF - 1) // 2)
-	
+	if skus_cnt.get('R', 0) >= 3:
+		freeQ = skus_cnt['R'] // 3
+		currQ = skus_cnt.get('Q', 0)
+		if currQ > 0:
+			price -= price_table['B'](currQ)
+			price += price_table['B'](currQ - freeQ)
 	return int(price)
+
