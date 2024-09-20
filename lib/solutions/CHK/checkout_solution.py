2885fbf04b76eb05bf85888448b3c215c72cab55
sbx_user1051 <sbx_user1051@169.254.101.229>
2024-09-20 19:04:31 +0000

    Fri Sep 20 19:04:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,22 @@
+from collections import Counter
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    p
-    raise NotImplementedError()
+	price_table = {
+		'A': lambda quantity: (quantity % 3) * 50 + (quantity // 3) * 130,
+		'B': lambda quantity: (quantity % 2) * 30 + (quantity // 2) * 45,
+		'C': lambda quantity: quantity * 20,
+		'D': lambda quantity: quantity * 15
+	}
+	if any([item not in price_table.keys() for item in skus]):
+		return -1
+	
+	skus_cnt = Counter(skus)
+	
+	price = 0
+	for item, quantity in skus_cnt.items():
+		price += price_table[item](quantity)
+	return price
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,8 +1,13 @@
 from solutions.CHK import checkout_solution
 
 
-class TestCheckout():
-    def test_sum(self):
-        sample = []
-        assert checkout_solution.checkout(sample) == 3
+class TestCheckout:
+    def test_simple(self):
+        assert checkout_solution.checkout(['A', 'A']) == 100
+
+    def test_ignore(self):
+        assert checkout_solution.checkout(['F']) == -1
+    
+    def test_complex(self):
+        assert checkout_solution.checkout(['A', 'A', 'A', 'A', 'B', 'D']) == 225
 
